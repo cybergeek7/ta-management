1dcd2c82b40218015631c95ceae33ff563ce3412
Fahad Altuhafi <faltuhafi@outlook.com>
2021-10-15 13:15:10 -0700

    v3

--- script.js
+++ script.js
@@ -15,29 +15,7 @@ const studentMajor = document.getElementById('major')
 const studentSelected = document.getElementById('selected')
 const logs = document.getElementById('logs')
 
-const students = [
-  {
-    firstName: 'Fahad',
-    lastName: 'Abdulhameed',
-    id: '45',
-    major: 'Programming',
-    selected: false,
-  },
-  {
-    firstName: 'Sara',
-    lastName: 'W',
-    id: '5',
-    major: 'Programming',
-    selected: false,
-  },
-  {
-    firstName: 'Jack',
-    lastName: 'A',
-    id: '325',
-    major: 'Programming',
-    selected: false,
-  },
-]
+const students = []
 
 let currentStudentIndex = 0
 let edit = false
